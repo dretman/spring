2a0050456b9e339e2024d16b3a601628e7160f32
Denis Retman <retman.denis@gmail.com>
2016-06-30 19:16:37 +0300

    Working with DB

---
+++ src/main/java/com/dataart/retman/domain/User.java
@@ -0,0 +1,34 @@
+package com.dataart.retman.domain;
+
+public class User {
+    private int id;
+    private String name;
+    private String surname;
+
+    public User(int id, String name, String surname) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                '}';
+    }
+}
