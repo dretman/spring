fbd4bc252accce135c5b733af69ab74796a677bb
Denis Retman <retman.denis@gmail.com>
2016-07-06 15:49:53 +0300

    Initial commit

--- src/main/java/com/dataart/retman/db/dao/SpitterDAO.java
+++
@@ -1,7 +0,0 @@
-package com.dataart.retman.db.dao;
-
-import com.dataart.retman.domain.Spitter;
-
-public interface SpitterDAO {
-    Spitter getSpitterById(long id);
-}
--- src/main/java/com/dataart/retman/db/repository/SpitterRepository.java
+++
@@ -1,9 +0,0 @@
-package com.dataart.retman.db.repository;
-
-import com.dataart.retman.domain.Spitter;
-
-public interface SpitterRepository {
-    void addSpitter(Spitter spitter);
-
-    Spitter retrieveSpitterById(long id);
-}
