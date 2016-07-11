d83f2819bc9182ac6c8a4495fd9afe0c5fa28fbe
Denis Retman <retman.denis@gmail.com>
2016-07-11 15:18:52 +0300

    Initial JPA commit

--- src/main/java/com/dataart/retman/Config.java
+++ src/main/java/com/dataart/retman/Config.java
@@ -1,48 +1,9 @@
 package com.dataart.retman;
 
-import org.apache.commons.dbcp.BasicDataSource;
-import org.springframework.beans.factory.config.BeanPostProcessor;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
-import org.springframework.orm.hibernate4.LocalSessionFactoryBean;
-
-import javax.sql.DataSource;
-import java.util.Properties;
 
 @Configuration
 public class Config {
 
-    @Bean
-    public BasicDataSource dataSource() {
-        BasicDataSource ds = new BasicDataSource();
 
-        ds.setDriverClassName("org.h2.Driver");
-        ds.setUrl("jdbc:h2:~/test");
-        ds.setUsername("sa");
-        ds.setPassword("");
-        ds.setInitialSize(5);
-        ds.setMaxActive(10);
-
-        return ds;
-    }
-
-    @Bean
-    public LocalSessionFactoryBean sessionFac(DataSource dataSource) {
-        LocalSessionFactoryBean sessionFactoryBean = new LocalSessionFactoryBean();
-        sessionFactoryBean.setDataSource(dataSource);
-        sessionFactoryBean.setMappingResources(new String[]{"/hibernate/Spitter.hbm.xml"});
-
-        Properties properties = new Properties();
-        properties.setProperty("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
-        properties.setProperty("show_sql", "true");
-        sessionFactoryBean.setHibernateProperties(properties);
-
-        return sessionFactoryBean;
-    }
-
-    @Bean
-    public BeanPostProcessor persistenceTranslation() {
-        return new PersistenceExceptionTranslationPostProcessor();
-    }
 }
