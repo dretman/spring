f6b54c4b8c2a25f159147f9b2917c6e3ad0630f5
Denis Retman <retman.denis@gmail.com>
2016-07-20 16:36:31 +0300

    DISPLAYING INTERNATIONALIZED MESSAGES

--- src/main/java/com/dataart/retman/RootConfig.java
+++ src/main/java/com/dataart/retman/RootConfig.java
@@ -4,11 +4,13 @@ import com.dataart.retman.repository.SpitterRepository;
 import com.dataart.retman.repository.SpittleRepository;
 import com.dataart.retman.repository.impl.SpitterRepositoryImpl;
 import com.dataart.retman.repository.impl.SpittleRepositoryImpl;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
@@ -23,6 +25,15 @@ public class RootConfig {
     @Bean
     public SpitterRepository spitterRepository() {
         return new SpitterRepositoryImpl();
+    }
+
+    @Bean
+    public MessageSource messageSource() {
+//        ResourceBundleMessageSource source = new ResourceBundleMessageSource();
+        ReloadableResourceBundleMessageSource source = new ReloadableResourceBundleMessageSource();
+        source.setBasename("file:///D:/PROJECTS/SPRING/messages");
+        source.setCacheSeconds(1);
+        return source;
     }
 
 }
