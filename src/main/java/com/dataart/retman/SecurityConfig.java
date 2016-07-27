4577bcbd29281436ec7cc1746d4e138ffa6d872d
Denis Retman <retman.denis@gmail.com>
2016-07-27 17:53:55 +0300

    Security initial commit

---
+++ src/main/java/com/dataart/retman/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.dataart.retman;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private static final String USER = "USER";
+    private static final String ADMIN = "ADMIN";
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("dretman").password("12345").roles(USER)
+                .and().withUser("admin").password("12345").roles(USER, ADMIN);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/spitter/**").authenticated()
+                .antMatchers(HttpMethod.POST, "/spittles/**").authenticated()
+                .anyRequest().permitAll();
+    }
+}
